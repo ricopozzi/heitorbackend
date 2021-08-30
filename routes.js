ea957cd121aff6d8bde0266f5770036ce60690b1
unknown <ricopozzi@hotmail.com>
2021-08-30 17:47:13 -0300

    awS

--- routes.js
+++ routes.js
@@ -3,6 +3,9 @@ const routes = express.Router()
 const emailSending = require('./contact')
 
 routes.post('/email', emailSending )
+routes.get('/', (req, res)=>{
+    res.json({message: 'server is Up'})
+})
 
 
 module.exports = routes 
