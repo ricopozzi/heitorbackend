9216ebba634a35b02d56c044575eb9161cfef9d7
unknown <ricopozzi@hotmail.com>
2021-08-30 14:47:45 -0300

    contact 35

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ const routes = require('./routes')
 app.use(express.json())
 
 app.use(cors())
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });   
+// app.use(function(req, res, next) {
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//     next();
+//   });   
 
 app.use(routes)
-
-
 
 app.listen(3333)
